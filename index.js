91036101cb48574c5ed096157d453730116961e3
Jill <jillmarie_inc0622@yahoo.com>
2018-04-05 23:57:19 +0800

    11th
    11th

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var express = require('express');
 var winston = require('winston');
 var app = express();
-var func = require("./app/index.js")(app);
+var func = require("app/index.js");
 func(app);
 
 app.listen(app.get('port'), () => {
