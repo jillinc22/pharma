dfd5637961b1665e254c532818d1ecaa434fe9c6
Jill <jillmarie_inc0622@yahoo.com>
2018-04-06 00:02:24 +0800

    13th
    13th

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 var express = require('express');
 var winston = require('winston');
 var app = express();
-var func = require("./app/index.js")(app);
-func();
+var func = require("./app/index.js")
+func(app);
 
 app.listen(app.get('port'), () => {
     winston.info(`Pharmastar Inventory is on PORT:${app.get('port')}`);
