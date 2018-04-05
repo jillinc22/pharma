277bcb8968e7d82c153967233cb453f132212119
Jill <jillmarie_inc0622@yahoo.com>
2018-04-06 00:46:48 +0800

    16th
    16th

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 var func = require("./app/index.js")
 func(app);
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+var port = process.env.PORT || 8000
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
